289f600dd2a79f1b5e3deb0b84ca83ca6eb4e345
Shreyanshu Sinha <128969488+shreyxnshu@users.noreply.github.com>
2024-04-06 22:50:31 +0530

    Add files via upload

---
+++ 60dass3.3.js
@@ -0,0 +1,15 @@
+function prime(num) {
+    for ( var i=0; i<=num; i++){
+        c=0
+        for ( var j=1; j<=i; j++){
+            if(i%j==0){
+                c++
+            }
+        }
+        if (c!=2){
+            console.log(i)
+        }
+    }
+}
+
+prime(15)
